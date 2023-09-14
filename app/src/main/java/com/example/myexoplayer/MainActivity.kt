c2894a56a1dabb7eed0148975c4a7cea5b6a79a6
Ravish Arora <5879193+ravarora@users.noreply.github.com>
2023-09-14 11:51:01 +0200

    Simple Exoplayer

--- app/src/main/java/com/example/myexoplayer/MainActivity.kt
+++ app/src/main/java/com/example/myexoplayer/MainActivity.kt
@@ -6,10 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import com.example.myexoplayer.ui.theme.MyExoPlayerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +35,69 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    
+                    ExoPlayerView(modifier = Modifier.fillMaxSize(), mediaUrl = stringResource(id = R.string.media_url_mp3))
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun ExoPlayerView(modifier: Modifier = Modifier, mediaUrl : String) {
+    val context = LocalContext.current
+    var mediaItemIndex by remember {
+        mutableStateOf(0)
+    }
+
+    var playbackPosition by remember {
+        mutableStateOf(0L)
+    }
+
+    var playWhenReady by remember {
+        mutableStateOf(false)
+    }
+
+    // create exo player
+    val exoplayer = remember {
+        ExoPlayer.Builder(context)
+            .build()
+            .also { exoPlayer ->
+                // create media item
+                val mediaItem = MediaItem.fromUri(mediaUrl)
+                exoPlayer.setMediaItems(listOf(mediaItem), mediaItemIndex, playbackPosition)
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.prepare()
+            }
+    }
+
+    val lifecycleOwner = rememberUpdatedState(LocalLifecycleOwner.current)
+
+    DisposableEffect(
+        AndroidView(modifier = modifier, factory = {
+            PlayerView(context).apply {
+                player = exoplayer
+            }
+        })
+    ) {
+        val observer = LifecycleEventObserver { _, event ->
+            when(event) {
+                //Lifecycle.Event.ON_RESUME -> exoplayer.play()
+                Lifecycle.Event.ON_PAUSE -> exoplayer.stop()
+                else -> {}
+            }
+        }
+
+        val lifecycle = lifecycleOwner.value.lifecycle
+        lifecycle.addObserver(observer)
+
+        onDispose {
+            exoplayer?.let {
+                playbackPosition = it.currentPosition
+                mediaItemIndex = it.currentMediaItemIndex
+                playWhenReady = it.playWhenReady
+            }
+            exoplayer.release()
+            lifecycle.removeObserver(observer)
         }
     }
 }
